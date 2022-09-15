501b5cb7a162a070dda171431169b737e0708929
Justin Lawson <jmlawson2002@gmail.com>
2022-09-15 12:14:17 -0400

    finished syntaz sugar excercise

---
+++ Syntax Sugar Excercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Syntax_Sugar_Excercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than of equal to nine";
+            Console.WriteLine(response);
+        }
+    }
+}
